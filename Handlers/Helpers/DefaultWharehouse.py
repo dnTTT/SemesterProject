c1b3ba5792bc279ea7356f140c1a9fe2b276a9ac
dnTTT <henriquevieira1805@hotmail.com>
2021-12-18 20:46:30 +0000

    FileHelper, DefaultWarehouse

--- Handlers/Helpers/DefaultWharehouse.py
+++ Handlers/Helpers/DefaultWharehouse.py
@@ -1,73 +1,39 @@
 import json
-from pathlib import Path
 import os
 from Models.Product import Product
-from os import path
+import Handlers.Helpers.FileHelper as FileHelper
 
 
 class DefaultWarehouse:
-    def __init__(self):
-        self.relativePath = os.path.join("Data", "Stock")
-        self.lastId = None
-        self.data = []
-        self.Initialize(self)
+	def __init__(self):
+		self.relativePath = os.path.join("Data", "Stock")
+		self.lastId = None
+		self.data = []
+		self.Initialize(self)
 
-    def AddProduct(self, NName, NPrice, NNumberInStock):
-        self.data.append(Product(NName, NPrice, NNumberInStock))
-        self.lastId += 1
+	def AddProduct(self, product_name, product_price, product_stock):
+		self.data.append(Product(product_name, product_price, product_stock))
+		self.lastId += 1
 
-    def CheckRelativePath(self):
-        current = os.getcwd()
-        folders = self.relativePath.Split("/").ToList()
-        try:
-            for folder in folders:
-                current += "/{}".format(folder)
-                if not path.exists(current):
-                    os.mkdir(current)
-            if path.exists((current + "/{}".format(self.relativePath))):
-                return True
-            else:
-                return False
-        except Exception as e:
-            raise Exception("Exception occurred when creating files directory", e)
+	def Initialize(self):
+		self.lastId = 1000
+		self.AddProduct("Product#1", 25.99, 100)
+		self.AddProduct("Product#2", 55.19, 60)
+		self.AddProduct("Product#3", 99.99, 40)
+		self.AddProduct("Product#4", 19.5, 300)
+		self.AddProduct("Product#5", 40.99, 70)
+		self.AddProduct("Product#6", 60, 100)
+		self.AddProduct("Product#7", 75.15, 45)
+		self.AddProduct("Product#8", 35.19, 110)
+		self.AddProduct("Product#9", 115.99, 30)
+		self.AddProduct("Product#10", 29.99, 120)
+		self.AddProduct("Luxury#1", 599.5, 10)
+		self.AddProduct("Luxury#2", 399.5, 20)
+		self.AddProduct("Luxury#3", 5599.5, 5)
+		self.AddProduct("Luxury#4", 3599.5, 5)
 
-    def AddOrOverwriteFile(self, fileName, jsonInput):
-        current_path = os.getcwd()
-        if not fileName.EndsWith(".json"):
-            fileName += ".json"
-        try:
-            if self.CheckRelativePath():
-                full_path = os.path.join(current_path, self.relativePath)
-                with open(full_path, 'w') as outfile:
-                    json.dump(jsonInput, outfile)
-                if os.path.isfile(full_path):
-                    return True
-                else:
-                    return False
-            else:
-                raise Exception("Unable to access proper directory. Relative path: {}".format(relativePath))
-        except Exception as e:
-            raise Exception("Unable to save data to a file", e)
+		for product in self.data:
+			json_string = json.dumps(product.__dict__)
+			FileHelper.add_or_overwrite_file(self.relative_path, product.name, json_string)
 
-    def Initialize(self):
-        self.lastId = 1000
-        self.AddProduct("Product#1", 25.99, 100)
-        self.AddProduct("Product#2", 55.19, 60)
-        self.AddProduct("Product#3", 99.99, 40)
-        self.AddProduct("Product#4", 19.5, 300)
-        self.AddProduct("Product#5", 40.99, 70)
-        self.AddProduct("Product#6", 60, 100)
-        self.AddProduct("Product#7", 75.15, 45)
-        self.AddProduct("Product#8", 35.19, 110)
-        self.AddProduct("Product#9", 115.99, 30)
-        self.AddProduct("Product#10", 29.99, 120)
-        self.AddProduct("Luxury#1", 599.5, 10)
-        self.AddProduct("Luxury#2", 399.5, 20)
-        self.AddProduct("Luxury#3", 5599.5, 5)
-        self.AddProduct("Luxury#4", 3599.5, 5)
-
-        for product in self.data:
-            json_string = json.dumps(product.__dict__)
-            self.AddOrOverwriteFile(product.name, json_string)
-
-        return True
+		return True
--- Handlers/Helpers/FileHelper.py
+++ Handlers/Helpers/FileHelper.py
@@ -1,1 +1,77 @@
+import os
+import json
 
+
+def add_or_overwrite_file(relative_path, file_name, json_input):
+	current_path = os.getcwd()
+	if not file_name.EndsWith(".json"):
+		file_name += ".json"
+	try:
+		if check_relative_path(relative_path):
+			full_path = os.path.join(current_path, relative_path)
+			with open(full_path, 'w') as outfile:
+				json.dump(json_input, outfile)
+			if os.path.isfile(full_path):
+				return True
+			else:
+				return False
+		else:
+			raise Exception("Unable to access proper directory. Relative path: {}".format(relative_path))
+	except Exception as e:
+		raise Exception("Unable to save data to a file", e)
+
+
+def get_object_from_file(relative_path, file_name):
+	try:
+		content = get_files_by_name(relative_path, file_name)
+		return json.load(content)
+
+	except Exception as e:
+		raise Exception(e)
+
+
+def get_files_by_name(relative_path, file_name, use_relative_path=True):
+	if not file_name.EndsWith(".json"):
+		file_name += ".json"
+
+	path = os.path.join(os.getcwd(), relative_path, file_name)
+
+	if use_relative_path is False:
+		path = relative_path
+	with open(path) as file_content:
+		lines = file_content.readlines()
+	return lines
+
+
+def get_all_data_from_directory(relative_path):
+	try:
+		base_dir = os.getcwd()
+		files = []
+		result = []
+		for dirpath, dirnames, file_names in os.walk(base_dir):
+			files.extend(file_names)
+
+		for f_name in files:
+			result.extend(json.load(get_files_by_name(f_name, "", False)))
+
+		return result
+
+	except Exception as e:
+		print(e)
+		return None
+
+
+def check_relative_path(relative_path):
+	current = os.getcwd()
+	folders = relative_path.Split("/").ToList()
+	try:
+		for folder in folders:
+			current += "/{}".format(folder)
+			if not os.path.exists(current):
+				os.mkdir(current)
+		if os.path.exists((current + "/{}".format(relative_path))):
+			return True
+		else:
+			return False
+	except Exception as e:
+		raise Exception("Exception occurred when creating files directory", e)
